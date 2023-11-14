4e1244e275e2eba160915bce953b5823fa35203d
karencfv <karencfv@users.noreply.github.com>
2023-11-14 16:55:39 +1300

    Polish API error handling

--- oxide/errors.go
+++ oxide/errors.go
@@ -1,6 +1,8 @@
 package oxide
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
@@ -16,7 +18,10 @@ type HTTPError struct {
 	StatusCode int
 	// Message is the server response message and is only populated when
 	// explicitly referenced by the JSON server response.
-	Message string
+	//Message string
+	// TODO: Add request method
+	//RequestMethod string
+
 	// Body is the raw response returned by the server.
 	// It is often but not always JSON, depending on how the request fails.
 	Body string
@@ -24,13 +29,50 @@ type HTTPError struct {
 	Header http.Header
 }
 
+// APIError is an error returned by a failed API call.
+type APIError struct {
+	// URL is the URL that was being accessed when the error occurred.
+	// It will always be populated.
+	URL *url.URL
+
+	// StatusCode is the HTTP response status code and will always be populated.
+	StatusCode int
+
+	RequestMethod string
+
+	ErrorResponse Error
+
+	// Header contains the response header fields from the server.
+	Header http.Header
+}
+
 // Error converts the Error type to a readable string.
 func (err HTTPError) Error() string {
-	if err.Message != "" {
-		return fmt.Sprintf("HTTP %d: %s (%s)", err.StatusCode, err.Message, err.URL)
-	}
+	//	if err.Message != "" {
+	//		return fmt.Sprintf("HTTP %d: %s (%s)", err.StatusCode, err.Message, err.URL)
+	//	}
 
 	return fmt.Sprintf("HTTP %d (%s) BODY -> %v", err.StatusCode, err.URL, err.Body)
+}
+
+// Error converts the Error type to a readable string.
+func (err APIError) Error() string {
+	output := new(bytes.Buffer)
+	fmt.Fprintln(output, "\n------- REQUEST -------")
+	fmt.Fprintf(output, "%s %s\n", err.RequestMethod, err.URL)
+
+	//	fmt.Fprintln(output, "------- HEADERS -------")
+	for k, v := range err.Header {
+		//fmt.Println(k, v)
+		fmt.Fprintf(output, "%s: %s\n", k, v)
+	}
+
+	fmt.Fprintln(output, "------- RESPONSE -------")
+	fmt.Fprintf(output, "Status: %d %s\n", err.StatusCode, err.ErrorResponse.ErrorCode)
+	fmt.Fprintf(output, "Message: %s\n", err.ErrorResponse.Message)
+	fmt.Fprintf(output, "RequestID: %s\n", err.ErrorResponse.RequestId)
+
+	return fmt.Sprintf("%v", output.String())
 }
 
 // checkResponse returns an error (of type *HTTPError) if the response
@@ -41,14 +83,23 @@ func checkResponse(res *http.Response) error {
 	}
 
 	slurp, _ := io.ReadAll(res.Body)
-	// TODO: We could optionally decode the response body as JSON if there is a
-	// standardized error format.
 
-	return &HTTPError{
-		URL:        res.Request.URL,
-		StatusCode: res.StatusCode,
-		Body:       string(slurp),
-		Header:     res.Header,
-		Message:    "",
+	var apiError Error
+	if err := json.Unmarshal([]byte(slurp), &apiError); err != nil {
+		return &HTTPError{
+			URL:        res.Request.URL,
+			StatusCode: res.StatusCode,
+			Body:       string(slurp),
+			Header:     res.Header,
+			//	Message:    "",
+		}
+	}
+
+	return &APIError{
+		RequestMethod: res.Request.Method,
+		StatusCode:    res.StatusCode,
+		URL:           res.Request.URL,
+		Header:        res.Header,
+		ErrorResponse: apiError,
 	}
 }
