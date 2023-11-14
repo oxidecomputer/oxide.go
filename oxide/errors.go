bddfeec881a498b348cc719c4cbc2240c97bed50
karencfv <karencfv@users.noreply.github.com>
2023-11-15 12:47:42 +1300

    Add moar tests

--- oxide/errors.go
+++ oxide/errors.go
@@ -6,60 +6,50 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-	"net/url"
 )
 
 // HTTPError is an error returned by a failed API call.
 type HTTPError struct {
-	// URL is the URL that was being accessed when the error occurred.
-	// It will always be populated.
-	URL *url.URL
-
-	// StatusCode is the HTTP response status code and will always be populated.
-	StatusCode int
-
-	// RequestMethod is the HTTP request method and will always be populated.
-	RequestMethod string
-
 	// ErrorResponse is the API's Error response type.
-	ErrorResponse *Error
+	ErrorResponse *ErrorResponse
 
-	// RawBody is the raw response returned by the server.
+	// HTTPResponse is the raw HTTP response returned by the server.
+	HTTPResponse *http.Response
+
+	// RawBody is the raw response body returned by the server.
 	RawBody string
-
-	// Header contains the response header fields from the server.
-	Header http.Header
 }
 
 // Error converts the HTTPError type to a readable string.
 func (err HTTPError) Error() string {
 	output := new(bytes.Buffer)
-	fmt.Fprintln(output, "\n------- REQUEST -------")
-	fmt.Fprintf(output, "%s %s\n", err.RequestMethod, err.URL)
+	fmt.Fprintf(output, "%s %s\n", err.HTTPResponse.Request.Method, err.HTTPResponse.Request.URL)
 
-	for k, v := range err.Header {
-		fmt.Fprintf(output, "%s: %s\n", k, v)
-	}
-
-	fmt.Fprintln(output, "------- RESPONSE -------")
+	fmt.Fprintln(output, "----------- RESPONSE -----------")
 	if err.ErrorResponse != nil {
-		fmt.Fprintf(output, "Status: %d %s\n", err.StatusCode, err.ErrorResponse.ErrorCode)
+		fmt.Fprintf(output, "Status: %d %s\n", err.HTTPResponse.StatusCode, err.ErrorResponse.ErrorCode)
 		fmt.Fprintf(output, "Message: %s\n", err.ErrorResponse.Message)
 		fmt.Fprintf(output, "RequestID: %s\n", err.ErrorResponse.RequestId)
 	} else {
 		// In the very unlikely case that the error response was not able to be parsed
 		// into oxide.Error, we will return the raw body of the response.
-		fmt.Fprintf(output, "Status: %d\n", err.StatusCode)
+		fmt.Fprintf(output, "Status: %d\n", err.HTTPResponse.StatusCode)
 		fmt.Fprintf(output, "Response Body: %v\n", err.RawBody)
+	}
+
+	fmt.Fprintln(output, "------- RESPONSE HEADERS -------")
+	for k, v := range err.HTTPResponse.Header {
+		fmt.Fprintf(output, "%s: %s\n", k, v)
 	}
 
 	return fmt.Sprintf("%v", output.String())
 }
 
+// TODO: export this function and change name
 // checkResponse returns an error (of type *HTTPError) if the response
 // status code is 3xx or greater.
 func checkResponse(res *http.Response) error {
-	if res.StatusCode <= 300 {
+	if res.StatusCode <= 299 {
 		return nil
 	}
 
@@ -68,15 +58,12 @@ func checkResponse(res *http.Response) error {
 	slurp, _ := io.ReadAll(res.Body)
 
 	e := HTTPError{
-		RequestMethod: res.Request.Method,
-		StatusCode:    res.StatusCode,
-		URL:           res.Request.URL,
-		Header:        res.Header,
-		RawBody:       string(slurp),
+		HTTPResponse: res,
+		RawBody:      string(slurp),
 	}
 
-	var apiError Error
-	if err := json.Unmarshal([]byte(slurp), &apiError); err != nil {
+	var apiError ErrorResponse
+	if err := json.Unmarshal(slurp, &apiError); err != nil {
 		// We return the error as is even with an unmarshal error,
 		// as it already contains the RawBody.
 		return e
--- oxide/errors_test.go
+++ oxide/errors_test.go
@@ -1,8 +1,10 @@
 package oxide
 
 import (
+	"io"
 	"net/http"
 	"net/url"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -10,51 +12,157 @@ import (
 
 func TestHTTPError_Error(t *testing.T) {
 	url, _ := url.Parse("http://127.0.0.1:12220/v1/disks/my-disk")
-	header := http.Header{}
-	header.Add("X-Request-Id", "99b40b0a-234f-4e10-87b4-081b0432ad19")
-	header.Add("Date", "Tue, 14 Nov 2023 06:57:09 GMT")
-	header.Add("Content-Length", "152")
+	// Go map iteration is random so we only add a single header
+	header := make(http.Header)
 	header.Add("Content-Type", "application/json")
+
+	res := http.Response{
+		StatusCode: 400,
+		Header:     header,
+		Request: &http.Request{
+			Method: http.MethodPost,
+			URL:    url,
+		},
+	}
+
+	apiErr := ErrorResponse{
+		ErrorCode: "ObjectAlreadyExists",
+		Message:   "already exists: project \"my-disk\"",
+		RequestId: "c42e6ade-69d5-4018-91f8-88bf53b5d026",
+	}
+
 	tests := []struct {
 		name   string
 		fields HTTPError
 		want   string
 	}{
 		{
+			name: "returns an error with populated oxide.Error type",
+			fields: HTTPError{
+				ErrorResponse: &apiErr,
+				RawBody:       "{error: Some error}",
+				HTTPResponse:  &res,
+			},
+			want: `POST http://127.0.0.1:12220/v1/disks/my-disk
+----------- RESPONSE -----------
+Status: 400 ObjectAlreadyExists
+Message: already exists: project "my-disk"
+RequestID: c42e6ade-69d5-4018-91f8-88bf53b5d026
+------- RESPONSE HEADERS -------
+Content-Type: [application/json]
+`,
+		},
+		{
 			name: "returns an error with raw body",
 			fields: HTTPError{
-				URL:           url,
-				StatusCode:    400,
-				RequestMethod: http.MethodDelete,
 				ErrorResponse: nil,
 				RawBody:       "{error: Some error}",
-				Header:        header,
+				HTTPResponse:  &res,
 			},
-			want: `
-------- REQUEST -------
-DELETE http://127.0.0.1:12220/v1/disks/my-disk
-X-Request-Id: [99b40b0a-234f-4e10-87b4-081b0432ad19]
-Date: [Tue, 14 Nov 2023 06:57:09 GMT]
-Content-Length: [152]
-Content-Type: [application/json]
-------- RESPONSE -------
+			want: `POST http://127.0.0.1:12220/v1/disks/my-disk
+----------- RESPONSE -----------
 Status: 400
 Response Body: {error: Some error}
+------- RESPONSE HEADERS -------
+Content-Type: [application/json]
 `,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			println(header)
 			err := HTTPError{
-				URL:           tt.fields.URL,
-				StatusCode:    tt.fields.StatusCode,
-				RequestMethod: tt.fields.RequestMethod,
 				ErrorResponse: tt.fields.ErrorResponse,
 				RawBody:       tt.fields.RawBody,
-				Header:        tt.fields.Header,
+				HTTPResponse:  tt.fields.HTTPResponse,
 			}
 			got := err.Error()
 			assert.Equal(t, tt.want, got)
+		})
+	}
+}
+
+func Test_checkResponse(t *testing.T) {
+	url, _ := url.Parse("http://127.0.0.1:12220/v1/disks/my-disk")
+	// Go map iteration is random so we only add a single header
+	header := make(http.Header)
+	header.Add("Content-Type", "application/json")
+
+	res := http.Response{
+		StatusCode: 300,
+		Header:     header,
+		Body:       io.NopCloser(strings.NewReader("some error")),
+		Request: &http.Request{
+			Method: http.MethodPost,
+			URL:    url,
+		},
+	}
+
+	res2 := http.Response{
+		StatusCode: 400,
+		Header:     header,
+		Body: io.NopCloser(strings.NewReader(`{
+  "request_id": "37a8ed33-b7ad-43b0-b2ce-1171d03f5324",
+  "error_code": "ObjectAlreadyExists",
+  "message": "already exists: project \"my-project\""
+}
+`)),
+		Request: &http.Request{
+			Method: http.MethodPost,
+			URL:    url,
+		},
+	}
+
+	tests := []struct {
+		name string
+		args *http.Response
+		want error
+	}{
+		{
+			name: "returns an error without populated oxide.Error type",
+			args: &res,
+			want: HTTPError{
+				HTTPResponse:  &res,
+				RawBody:       "some error",
+				ErrorResponse: nil,
+			},
+		},
+		{
+			name: "returns an error with populated oxide.Error type",
+			args: &res2,
+			want: HTTPError{
+				HTTPResponse: &res2,
+				RawBody: `{
+  "request_id": "37a8ed33-b7ad-43b0-b2ce-1171d03f5324",
+  "error_code": "ObjectAlreadyExists",
+  "message": "already exists: project \"my-project\""
+}
+`,
+				ErrorResponse: &ErrorResponse{
+					Message:   "already exists: project \"my-project\"",
+					ErrorCode: "ObjectAlreadyExists",
+					RequestId: "37a8ed33-b7ad-43b0-b2ce-1171d03f5324",
+				},
+			},
+		},
+		{
+			name: "returns nil when is success response",
+			args: &http.Response{
+				StatusCode: 200,
+				Header:     header,
+				Body:       io.NopCloser(strings.NewReader("success")),
+				Request: &http.Request{
+					Method: http.MethodPost,
+					URL:    url,
+				},
+			},
+			want: nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := checkResponse(tt.args)
+			assert.Equal(t, tt.want, err)
 		})
 	}
 }
