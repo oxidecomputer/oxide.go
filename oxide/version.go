161e83aa683266045c813496bc03bbc670eb7d13
Matthew Sanabria <matthew.sanabria@oxide.computer>
2026-09-01 13:41:06 -0400

    misc: bump to next development version
    Bumped to v0.12.0, the next development version.

--- oxide/version.go
+++ oxide/version.go
@@ -8,7 +8,7 @@ package oxide
 
 // version is the Oxide Go SDK version. This is used to dynamically populate the
 // user agent for [Client].
-const version = "v0.11.0"
+const version = "v0.12.0"
 
 // APIVersion is the OpenAPI specification version the Oxide Go SDK was
 // generated from. This is used to populate the 'API-Version' header for
